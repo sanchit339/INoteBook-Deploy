39ac0515768636597bee0f29159f66a66e1a6282
Sanchit Ingale <89060338+sanchit339@users.noreply.github.com>
2024-03-05 23:52:47 +0530

    Chnages

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -33,7 +33,7 @@ function App() {
     <div className="container">
       <Routes>
           <Route path="/" element={<Home showAlert={showAlert} />} />
-          <Route path="about/*" element={<About />} />
+          {/* <Route path="about/*" element={<About />} /> */}
           <Route path="login/*" element={<Login showAlert={showAlert} />} />
           <Route path="signup/*" element={<Signup showAlert={showAlert} />} />
       </Routes>
